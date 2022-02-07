2b38ceddcb33e8d65a879f6d15eba78b9a6de1ef
David Sullo <dsullo@bu.edu>
2022-02-07 15:14:01 -0500

    Added add/remove for checker to modify board

--- src/Board.java
+++ src/Board.java
@@ -31,8 +31,27 @@ public class Board {
         return s;
     }
 
+    // Valids parameters and procedes to add the desired checker to the Board
+    public void addChecker(char checker, int row, int col) {
+        assert (checker == 'X' || checker == 'O'): "Invalid Checker Type";
+        assert (row < this.board.length && col < this.board[0].length): "Index is Out of Range";
+        assert (this.board[row][col] == '\u0000'): "Space is Already Taken";
+        this.board[row][col] = checker;
+    }
+
+    // Valids parameters and procedes to remove the desired index's checker from the Board
+    public void removeChecker(int row, int col) {
+        assert (row < this.board.length && col < this.board[0].length): "Index is Out of Range";
+        this.board[row][col] = '\u0000';
+    }
+
     public static void main(String[] args) {
         Board test = new Board();
+        test.addChecker('X', 1, 1);
+        test.addChecker('O', 2, 2);
         System.out.println(test);
+        test.removeChecker(2, 2);
+        System.out.println(test);
+        test.removeChecker(5, 5);
     }
 }

bc9b897b0296a9c6b369bcd1738196360d4e4e6d
David Sullo <dsullo@bu.edu>
2022-02-07 17:45:40 -0500

    Added toString and getters for Player class

--- src/Player.java
+++ src/Player.java
@@ -10,4 +10,24 @@ public class Player {
         this.checker = checker;
         this.numWins = 0;
     }
+
+    public String toString() {
+        return "Player " + checker;
+    }
+
+    public char getChecker() {
+        return checker;
+
+    }
+    
+    public int getNumWins() {
+        return this.numWins;
+    }
+
+    public static void main(String[] args) {
+        Player test = new Player('X');
+        System.out.println(test);
+        System.out.println(test.getChecker());
+        System.out.println(test.getNumWins());
+    }
 }

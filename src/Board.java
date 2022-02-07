a7fc528d830e326470862ee02c76dce094e9eefb
David Sullo <dsullo@bu.edu>
2022-02-07 17:33:07 -0500

    Added method checking for all possible wins

--- src/Board.java
+++ src/Board.java
@@ -45,13 +45,38 @@ public class Board {
         this.board[row][col] = '\u0000';
     }
 
+    // Returns true if specified char has a win in the game
+    public boolean isWin(char checker) {
+        assert (checker == 'X' || checker == 'O'): "Invalid Checker Type";
+        // loops through possible row/col indices. checks for horizontal/vertical win each pass for efficiency
+        for (int i = 0; i < this.board.length; i++) {
+            // checks for horizontal win
+            if (this.board[i][0] == checker && this.board[i][1] == checker && this.board[i][2] ==  checker) {
+                return true;
+            }
+            // checks for vertical win
+            if (this.board[0][i] == checker && this.board[1][i] == checker && this.board[2][i] ==  checker) {
+                return true;
+            }
+        }
+        // checks for upper-left to lower-right diagonal win
+        if (this.board[0][0] == checker && this.board[1][1] == checker && this.board[2][2] ==  checker) {
+            return true;
+        }
+        // checks for upper-right to lower-left diagonal win
+        if (this.board[0][2] == checker && this.board[1][1] == checker && this.board[2][0] ==  checker) {
+            return true;
+        }
+        // returns false if no win is found
+        return false;
+    }
+
     public static void main(String[] args) {
         Board test = new Board();
+        test.addChecker('X', 0, 2);
         test.addChecker('X', 1, 1);
-        test.addChecker('O', 2, 2);
+        test.addChecker('X', 2, 0);
         System.out.println(test);
-        test.removeChecker(2, 2);
-        System.out.println(test);
-        test.removeChecker(5, 5);
+        System.out.println(test.isWin('X'));
     }
 }
